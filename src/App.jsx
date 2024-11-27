818d8df1cc1cf3fb810d48e7ab6b895e7b45edf0
Hafeez Ur Rahman <khan3344kpk@gmail.com>
2024-11-27 19:31:24 +0500

    Add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./Components/Home/Home";
 import NavBar from "./Components/NabBar/NavBar";
 import Footer from "./Components/Footer/Footer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SideBar from "./Components/Sidebar/SideBar";
 import TransactionsTable from "./Pages/Transaction-Table/TransactionsTable";
 import SupportHelpCenter from "./Pages/Support-Help-Center/SupportHelpCenter";
@@ -15,17 +15,38 @@ import UnverifiedTransactions from "./Pages/Unverified-Transactions/UnverifiedTr
 import DeclinedTransactions from "./Pages/Declined-Transactions/DeclinedTransactions";
 import UploadStatement from "./Pages/Upload-Statement/UploadStatement";
 import ReportsAndAnalytics from "./Pages/Reports-&-Analytics/ReportsAndAnalytics";
+import Login from "./Pages/Admin-Login/Login";
 
 function App() {
+  const [hideSidebar, setHideSidebar] = useState(false);
+  const [hideNavbar, setHideNavbar] = useState(false);
+  const [hideFooter, setHideFooter] = useState(false);
   const [showSidebar, setShowSide] = useState(
     window.innerWidth > 760 ? true : false
   );
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/login") {
+      setHideSidebar(true);
+      setHideNavbar(true);
+      setHideFooter(true);
+    } else {
+      setHideSidebar(false);
+      setHideNavbar(false);
+      setHideFooter(false);
+    }
+  }, [location]);
   return (
     <>
-      <SideBar showSidebar={showSidebar} setShowSide={setShowSide} />
+      {!hideSidebar && (
+        <SideBar showSidebar={showSidebar} setShowSide={setShowSide} />
+      )}
       <div>
-        <NavBar setShowSide={setShowSide} showSidebar={showSidebar} />
+        {!hideNavbar && (
+          <NavBar setShowSide={setShowSide} showSidebar={showSidebar} />
+        )}
         <Routes>
+          <Route path="/login" element={<Login showSidebar={showSidebar} />} />
           <Route path="/" element={<Home showSidebar={showSidebar} />} />
           <Route
             path="/TransactionsTable"
@@ -51,7 +72,7 @@ function App() {
             path="/MerchantManagement"
             element={<MerchantManagement showSidebar={showSidebar} />}
           />
-           <Route
+          <Route
             path="/ReportsAndAnalytics"
             element={<ReportsAndAnalytics showSidebar={showSidebar} />}
           />
@@ -70,7 +91,9 @@ function App() {
             element={<UploadStatement showSidebar={showSidebar} />}
           />
         </Routes>
-        <Footer showSide={setShowSide} showSidebar={showSidebar} />
+        {!hideFooter && (
+          <Footer showSide={setShowSide} showSidebar={showSidebar} />
+        )}
       </div>
     </>
   );
