b3ab4aeab87e7c226afb00d76b472a1c4429916b
IRFAN UL HAQ <khan3344kpk@gmail.com>
2024-12-08 19:12:17 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const BACKEND_URL = "http://192.168.1.13:8888";
+const BACKEND_URL = "http://192.168.1.4:8888";
 
 // ------------------------------------- Merchant Login api------------------------------------
 export const fn_loginMerchantApi = async (data) => {
