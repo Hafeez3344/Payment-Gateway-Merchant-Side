ea4255cd9f4767f8f3cf691cc0ec551a3db61be0
Hafeez3344 <khan3344kpk@gmail.com>
2025-02-21 22:08:29 +0500

    add footer change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = ({ showSidebar }) => {
   return (
     <div className={`h-[50px] bg-white flex justify-center items-center`}>
-        <p className="text-[#7987A1] font-[500] text-[13px] sm:text-[15px]"> Copyright © 2024 BetPay. All rights reserved.</p>
+      <p className="text-[#7987A1] font-[500] text-[13px] sm:text-[15px]"> Copyright © 2024 Royal247. All rights reserved.</p>
     </div>
   );
 };
