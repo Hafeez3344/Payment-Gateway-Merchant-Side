d3676c96921d253d15091f6615c17dc431e85347
Hafeez3344 <khan3344kpk@gmail.com>
2025-03-20 02:23:58 +0500

    footer text change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = ({ showSidebar }) => {
   return (
     <div className={`h-[50px] bg-white flex justify-center items-center`}>
-      <p className="text-[#7987A1] font-[500] text-[13px] sm:text-[15px]"> Copyright © 2024 Royal247. All rights reserved.</p>
+      <a
+        href="https://netrex.ae"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-[#7987A1] font-[500] text-[13px] sm:text-[15px] hover:text-[#5d6b82] cursor-pointer"
+      >
+        Developed by NETREX Inc. All rights reserved
+      </a>
     </div>
   );
 };
